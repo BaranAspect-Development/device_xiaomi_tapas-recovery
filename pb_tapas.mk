dc921af77b7f584562bda8f4051e960b21fc6e69
chickendrop89 <chickendrop89@gmail.com>
2024-07-23 11:39:28 +0000

    recovery: Welcome PBRP!
    Also fix flashlight, and some other things...

--- pb_tapas.mk
+++ pb_tapas.mk
@@ -11,14 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Inherit from device configuration
 $(call inherit-product, device/xiaomi/tapas/device.mk)
 
-# Inherit from TWRP common configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from PBRP common configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := tapas
-PRODUCT_NAME := twrp_tapas
+PRODUCT_NAME := pb_tapas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 4G
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# PBRP - Flashlight path
+PB_TORCH_PATH := "/system/etc/flashlight"
+
+# PBRP - Disable flashlight brightness slider
+PB_TORCH_MAX_BRIGHTNESS := 1
+
+# PBRP - Disable Treble compatibility check
+PB_DISABLE_DEFAULT_TREBLE_COMP := true
